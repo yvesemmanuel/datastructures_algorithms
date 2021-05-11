6778ebaf3478202d695dd1616db828f581887cbd
Yves Emmanuel <yefo@cin.ufpe.br>
2021-05-10 21:29:54 -0300

    insertion sort implementation

---
+++ algorithms/insertion sort (decrease-and-conquer).cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+template <typename E>
+void inssort(E A[], int n) {
+	for (int i = 1; i < n; i++)
+		for (int j = i; (j > 0) && (A[j] < A[j - 1]); j--)
+			swap(A[j], A[j - 1]);
+}
+
+int main() {
+	int A[5] = {8, 31, 3, 49, 98};
+	inssort(A, 5);
+	
+	for (int i = 0; i < 5; i++)
+		cout << A[i] << " ";
+	
+	return 0;
+}
+
+

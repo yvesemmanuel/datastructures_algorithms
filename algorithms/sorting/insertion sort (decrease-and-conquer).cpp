28aa4746912b79d6aafd5df5344b01cd25913474
Yves Emmanuel <yefo@cin.ufpe.br>
2021-05-15 19:32:16 -0300

    insertion sort modification

--- algorithms/sorting/insertion sort (decrease-and-conquer).cpp
+++ algorithms/sorting/insertion sort (decrease-and-conquer).cpp
@@ -3,9 +3,17 @@ using namespace std;
 
 template <typename E>
 void inssort(E A[], int n) {
-	for (int i = 1; i < n; i++)
-		for (int j = i; (j > 0) && (A[j] < A[j - 1]); j--)
-			swap(A[j], A[j - 1]);
+	int i, j;
+	E v;
+    for (i = 1; i < n; i++) {
+        v = A[i];
+        j = i - 1;
+        while ((j >= 0) && (A[j] > v)) {
+            A[j + 1] = A[j];
+            j = j - 1;
+        }
+        A[j + 1] = v;
+    }
 }
 
 int main() {
